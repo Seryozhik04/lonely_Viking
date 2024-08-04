5fac53a9f686ccb605a5b1545152a5f7e24bcaca
ЁЖИК <GavrilovSergeyDenisovich@outlook.com>
2024-08-04 18:33:39 +1000

    Record Score
    Рекордный счёт. Обновляется, если набито больше очков.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,10 +9,20 @@ public class GameManager : MonoBehaviour
     public GameObject menu;
     public GameObject joystic;
 
+    private int recordScore;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("SavedScore"))
+        {
+            recordScore = PlayerPrefs.GetInt("SavedScore");
+        }
+        else recordScore = 0;
+        GameObject.Find("RecordScore").GetComponent<Text>().text = "Record Score: " + recordScore;
+    }
     public void EndGame()
     {
-        movement.enabled = false;
-
+        movement.enabled = false; 
         Invoke("RestartLevel", levelRestartDelay);
     }
     public void Conflict(bool start)
@@ -20,6 +31,15 @@ public class GameManager : MonoBehaviour
         {
             menu.transform.position = Camera.main.transform.position + new Vector3(0,0,1);
             joystic.SetActive(false);
+
+            int score = System.Int32.Parse(GameObject.Find("Score").GetComponent<Text>().text);
+            if (score > recordScore)
+            {
+                recordScore = score;
+                GameObject.Find("RecordScore").GetComponent<Text>().text = "Record Score: " + recordScore;
+                PlayerPrefs.SetInt("SavedScore", recordScore);
+                PlayerPrefs.Save();
+            }
         }
         else
         {
